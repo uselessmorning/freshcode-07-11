df729768913bc1d5974243759cafefcc40432881
uselessmorning <thebigtonny3@gmail.com>
2022-11-07 11:28:40 +0200

    Add commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let number1 = 12;
-let number2 = '12';
+// let number1 = 12;
+// let number2 = '12';
 
-console.log (number1+number2); //неявне приведення number1 до строки
-console.log (number1+Number(number2)); // явне приведення number2 до числа
+// console.log (number1+number2); //неявне приведення number1 до строки
+// console.log (number1+Number(number2)); // явне приведення number2 до числа
 
-number1 = 5;
-number2 ='';
-console.log (number1*number2);
+// number1 = 5;
+// number2 ='';
+// console.log (number1*number2);
 
-//false 
+// //false 
 //0 -0 
 //NaN
 // ''
