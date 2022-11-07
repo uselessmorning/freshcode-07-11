eff567e4439fdf3f0a02d8e1e94cf34f15243432
uselessmorning <thebigtonny3@gmail.com>
2022-11-07 11:03:50 +0200

    Add example conversion

---
+++ index.js
@@ -0,0 +1,17 @@
+let number1 = 12;
+let number2 = '12';
+
+console.log (number1+number2); //неявне приведення number1 до строки
+console.log (number1+Number(number2)); // явне приведення number2 до числа
+
+number1 = 5;
+number2 ='';
+console.log (number1*number2);
+
+//false 
+//0 -0 
+//NaN
+// ''
+//undefined
+//null
+//приведення цих випадків до булевого випадку, завжди дає false 
